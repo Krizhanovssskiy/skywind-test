a6218a5b4da49ffe5f18fc791101df10424ff70c
krizhanovssskiy <krizhanovssskiy@gmail.com>
2021-04-30 01:19:18 +0300

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+
+import {
+  createStyles,
+  fade,
+  Theme,
+  ThemeProvider,
+  withStyles,
+  makeStyles,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import SaveIcon from '@material-ui/icons/Save';
 import { Container } from '@material-ui/core';
 import Table from "./components/table"
 import AddCircleOutlineSharpIcon from '@material-ui/icons/AddCircleOutlineSharp';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import './App.scss';
+
+const ValidationTextField = withStyles({
+  root: {
+    '& input:valid + fieldset': {
+      borderColor: 'green',
+      borderWidth: 2,
+    },
+    // '& input:invalid + fieldset': {
+    //   borderColor: 'red',
+    //   borderWidth: 2,
+    // },
+    '& input:valid:focus + fieldset': {
+      borderLeftWidth: 6,
+      padding: '4px !important', // override inline-style
+    },
+  },
+})(TextField);
 
 function App() {
   return (
@@ -14,6 +44,29 @@ function App() {
         </button>
 
         <Table/>
+
+        <form 
+        // className={classes.root}
+         noValidate autoComplete="off">
+          <ValidationTextField
+            // className={classes.margin}
+            label="CSS validation style"
+            required
+            variant="outlined"
+            defaultValue=""
+            id="validation-outlined-input"
+
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            // className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Save
+          </Button>
+        </form>
       </Container>
     </div>
   );
